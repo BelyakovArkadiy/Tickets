98945087d2de61e7846268d7f50a1ab3b4062040
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-02-14 19:21:14 +0600

    new file:   src/main/java/ru/netology/domain/TicketByTimeAscComparator.java 	modified:   src/main/java/ru/netology/manager/TicketManager.java 	modified:   src/test/java/ru/netology/manager/TicketManagerTest.java

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -3,7 +3,8 @@ package ru.netology.manager;
 import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
-
+import java.util.Arrays;
+import java.util.Comparator;
 
 
 public class TicketManager {
@@ -11,8 +12,11 @@ public class TicketManager {
     public TicketManager(TicketRepository repository) {
         this.repository = repository;
     }
-    public Ticket[] searchBy(String from, String to) {
+
+    public Ticket[] searchBy(String from, String to, Comparator<Ticket> comparator) {
+
         Ticket[] result = new Ticket[0];
+
         for (Ticket ticket : repository.findAll()) {
             if (matches(ticket, from, to)) {
                 Ticket[] tmp = new Ticket[result.length + 1];
@@ -21,6 +25,7 @@ public class TicketManager {
                 result = tmp;
             }
         }
+        Arrays.sort(result, comparator);
         return result;
     }
     public boolean matches(Ticket ticket, String from, String to) {
--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -2,23 +2,28 @@ package ru.netology.manager;
 
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Ticket;
+import ru.netology.domain.TicketByTimeAscComparator;
 import ru.netology.repository.TicketRepository;
 import ru.netology.repository.TicketRepository;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicketManagerTest {
 
     private TicketRepository repository = new TicketRepository();
-    private Ticket ticket1 = new Ticket(1, 200, "ALA", "SWE", 350);
-    private Ticket ticket2 = new Ticket(2, 500, "ALA", "SWE", 300);
+    private Ticket ticket1 = new Ticket(1, 200, "ALA", "SWE", 300);
+    private Ticket ticket2 = new Ticket(2, 300, "ALA", "SWE", 350);
     private Ticket ticket3 = new Ticket(3, 400, "ALA", "SWE", 360);
     private Ticket ticket4 = new Ticket(4, 700, "ALA", "SIP", 300);
-    private Ticket ticket5 = new Ticket(5, 200, "ALA", "SWE", 410);
+    private Ticket ticket5 = new Ticket(5, 200, "ALA", "SWE", 120);
     private Ticket ticket6 = new Ticket(6, 600, "ALA", "SSE", 400);
+    TicketByTimeAscComparator comparator = new TicketByTimeAscComparator();
+
+
 
 
     @Test
@@ -31,9 +36,11 @@ class TicketManagerTest {
         repository.save(ticket6);
 
         TicketManager manager = new TicketManager(repository);
-        Ticket[] actual = manager.searchBy("ALA", "SWE");
-        Ticket[] expected = new Ticket[]{ticket1, ticket5, ticket3, ticket2};
-        Arrays.sort(actual);
+
+        Ticket[] actual = manager.searchBy("ALA", "SWE",comparator);
+        Ticket[] expected = new Ticket[]{ticket5, ticket1, ticket2, ticket3};
+
+
         assertArrayEquals(actual, expected);
 
 
@@ -44,9 +51,9 @@ class TicketManagerTest {
         repository.save(ticket5);
 
         TicketManager manager = new TicketManager(repository);
-        Ticket[] actual = manager.searchBy("ALA", "SWE");
+        Ticket[] actual = manager.searchBy("ALA", "SWE",comparator);
         Ticket[] expected = new Ticket[]{ticket5};
-        Arrays.sort(actual);
+
         assertArrayEquals(actual, expected);
 
     }
@@ -58,9 +65,9 @@ class TicketManagerTest {
         repository.save(ticket6);
 
         TicketManager manager = new TicketManager(repository);
-        Ticket[] actual = manager.searchBy("ALA", "SWE");
+        Ticket[] actual = manager.searchBy("ALA", "SWE",comparator);
         Ticket[] expected = new Ticket[]{};
-        Arrays.sort(actual);
+
         assertArrayEquals(actual, expected);
 
     }
@@ -72,9 +79,9 @@ class TicketManagerTest {
 
 
         TicketManager manager = new TicketManager(repository);
-        Ticket[] actual = manager.searchBy("ALA", "SWE");
-        Ticket[] expected = new Ticket[]{ticket1, ticket5}; // два билета с одинаковой стоимостью, сортировка происходит по индексу
-        Arrays.sort(actual);
+        Ticket[] actual = manager.searchBy("ALA", "SWE",comparator);
+        Ticket[] expected = new Ticket[]{ticket5, ticket1}; // два билета с одинаковой стоимостью
+
         assertArrayEquals(actual, expected);
 
     }
